711d82be5585d83f737a3b5de52cb8050684ace4
Andres Romero <andres.romero.otalora@gmail.com>
2020-11-19 00:16:30 -0500

    clase 18: firebase

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -1,77 +1,32 @@
-module.exports = function(databaseConfig) {
+module.exports=function (databaseConfig) {
     const express = require('express');
     const router = express.Router();
     const TABLE = 'users';
     let model;
-     
+
     switch (databaseConfig.default) {
         case 'mongodb':
-            model = require('../models/mongodb-model')(databaseConfig.mongodb,databaseConfig.mongodb_url);
+            model = require('../models/mongodb-model')(databaseConfig.mongodb, databaseConfig.mongodb_url);
             break;
-        
         case 'sqlite':
-            model = require('../models/sqlite-model')(databaseConfig.sqlite); 
+            model = require('../models/sqlite-model')(databaseConfig.sqlite);
             break;
-    
+        case 'firestore':
+            model = require('../models/firestore-model')(databaseConfig.firestore);
+            break;
         default:
-            model = require('../models/sqlite-model')(databaseConfig.sqlite); 
+            model = require('../models/sqlite-model')(databaseConfig.sqlite);
             break;
     }
 
-    //{{SERVER}}/users/lista todos los usuarios
-    router.get("/", function (request, response){
-        model.getAll(TABLE)
-        .then((rows)=>{
-            response.send(rows);
-        }).catch((error)=>{
-            console.error(error);
-            response.send(error);
-        });
-    });
-
-    //{{SERVER}}/users/id
-    //trae un usuario por ID
-    router.get('/:id', function (request, response){
-        let id = request.params.id;
-        model.getById(TABLE, id)
-        .then((row)=>{
-            response.send(row);
-        }).catch((error)=>{
-            console.error(error);
-            response.send(error);
-        });
-    });
+    //
+    
 
     //{{SERVER}}/users/
-    //Crea un usuario
-    router.post('/', function(request,response){
-        model.create(TABLE, request.body)
-        .then((object)=>{
-            response.send(object)
-        }).catch((error)=>{
-            console.error(error);
-            response.send(error);
-        });
-    });
 
-    //{{SERVER}}/users/:id
-    //Edita un usuario pór ID
-    router.put('/:id', function(request,response){
-        let id = request.params.id;
-        model.update(TABLE,request.body, id)
-        .then((row)=>{
-            response.send(row);
-        }).catch((error)=>{
-            console.error(error);
-            response.send(error);
-        });
-    });
-    
-    //{{SERVER}}/users/:id
-    //Elimina un usuario por ID
-    router.delete('/:id', function(request, response){
-        let id = request.params.id;
-        model.delete(TABLE, id)
+    //{{SERVER}}/users/create_users
+    router.post('/options/initialize', function(request, response){
+        model.initialize(TABLE, request.body)
         .then((message)=>{
             response.send(message);
         }).catch((error)=>{
@@ -79,10 +34,9 @@ module.exports = function(databaseConfig) {
             response.send(error);
         });
     });
-
+    
     //{{SERVER}}/users/delete_users
-    //Limpiar tabla
-    router.get('/option/clean', function(request, response){
+    router.get('/options/clean', function(request, response){
         model.clean(TABLE)
         .then((message)=>{
             response.send(message);
@@ -91,19 +45,61 @@ module.exports = function(databaseConfig) {
             response.send(error);
         });
     });
-
-
-    //{{SERVER}}/users/create_users
-    router.post('/option/initialize', function (request, response) {
-        model.initialize(TABLE, request.body)
-        .then((message)=>{
-            response.send(message)
+    
+    //{{SERVER}}/users/
+    router.post('/', function(request,response){
+        model.create(TABLE, request.body)
+        .then((object)=>{
+            response.send(object);
         }).catch((error)=>{
             console.error(error);
             response.send(error);
         });
-        
+    });  
+
+    router.put('/:id', function(request,response){
+        let id = request.params.id;
+        model.update(TABLE, request.body, id)
+        .then((row)=>{
+            response.send(row);
+        }).catch((error)=>{
+            console.error(error);
+            response.send(error);
+        });        
     });
     
+    router.get('/', function(request, response){
+        model.getAll(TABLE)
+        .then((rows)=>{
+            response.send(rows);
+        }).catch((error)=>{
+            console.error(error);
+            response.send(error);
+        });
+    });   
+
+
+    router.get('/:id', function (request, response) {
+        let id = request.params.id;
+        model.getById(TABLE, id)
+        .then((row)=>{
+            response.send(row);
+        }).catch((error)=>{
+            console.error(error);
+            response.send(error);
+        });
+    });   
+
+    router.delete('/:id', function (request, response) {
+        let id = request.params.id;
+        model.delete(TABLE, id)
+        .then((message)=>{
+            response.send(message);
+        }).catch((error)=>{
+            console.error(error);
+            response.send(error);
+        });
+    });      
+
     return router;
 }
--- index.js
+++ index.js
@@ -1,32 +1,41 @@
-const express = require('express'); //creación de una variable llamada express
-const app = express();       //instancia de express  
+const express = require('express');
+const app = express();
 const bodyParser = require('body-parser');
-const sqlite3 = require('sqlite3').verbose(); //permite registrar todos los cambios realizado en
-                                               //la BD logs 
-let sqliteClient = new sqlite3.Database('./db/db.sqlite3');   
-const mongodbCliente = require('mongodb').MongoClient;
+//SQL Support
+const sqlite3 = require('sqlite3').verbose();
+const sqliteClient = new sqlite3.Database('./db/db.sqlite');
+
+//MongoDB Support
+const mongodbClient = require('mongodb').MongoClient;
 const url = 'mongodb://localhost:27017';
+
+//Firebase Support
+const admin = require("firebase-admin");
+const serviceAccount = require("./private/key.json");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://gestion-parques.firebaseio.com"
+});
+const firestore = admin.firestore();
+
 
 const databaseConfig = {
     "sqlite":sqliteClient,
-    "mongodb": mongodbCliente,
+    "mongodb":mongodbClient,
     "mongodb_url":url,
+    "firestore": firestore,
     "default":'mongodb'
-};
+}
 
-let usersController = require('./app/controllers/users')(databaseConfig);
+let usersController= require('./app/controllers/users')(databaseConfig);
 let classesController = require('./app/controllers/classes')(databaseConfig);
 
-
-
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-//{{SERVER}}/users/
-app.use('/users', usersController);
-app.use('/classes', classesController);
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use('/users',usersController);
+app.use('/classes',classesController);
+   
 
-
-
-app.listen(3455, function (){
-    console.log('corriendo')
+app.listen(3455, function(){
+    console.log('corriendo');
 });
