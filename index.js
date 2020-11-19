860dd53f4411c2db041842a77bb46dd6de77d890
Andres Romero <andres.romero.otalora@gmail.com>
2020-11-19 16:43:55 -0500

    clase 21: jwt controller user-firebase

--- index.js
+++ index.js
@@ -1,30 +1,25 @@
 const express = require('express');
 const app = express();
-var jwt = require('jsonwebtoken');
+//var jwt = require('jsonwebtoken');
+
+
 const bodyParser = require('body-parser');
+//const { JsonWebTokenError } = require('jsonwebtoken');
 
-//const general = require('./app/utils/general')();
-
-
-let usersController= require('./app/controllers/users')();
+let usersController = require('./app/controllers/users_firebase')();
 let classesController = require('./app/controllers/classes')();
+let loginController = require('./app/controllers/login')();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true}));
-app.use('/users',usersController);
-app.use('/classes',classesController);
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/',function(request,response){
-  let datos = {
-    name: "Soy un dato",
-    date: new Date()
-  }
-  let secreto = '1234568';
 
-  let token = jwt.sign(datos,secreto);
-  response.send(token);
-});   
 
-app.listen(3455, function(){
+app.use('/users', usersController);
+app.use('/classes', classesController);
+app.use('/login', loginController);
+
+
+app.listen(3455, function () {
     console.log('corriendo');
 });
