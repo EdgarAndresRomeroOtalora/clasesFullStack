d16d186c19df76d9349cecc43cbbf6ba780340b8
Andres Romero <andres.romero.otalora@gmail.com>
2020-11-22 18:15:50 -0500

    clase 20

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const bodyParser = require('body-parser');
 const { response } = require('express');
 
 
-//const general = require('./app/utils/general')();
 
 
 let usersController = require('./app/controllers/users')();
