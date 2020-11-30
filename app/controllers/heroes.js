ce50566e66fbba450dd6b975d2fee48952b9dcd0
Andres Romero <andres.romero.otalora@gmail.com>
2020-11-30 02:48:11 -0500

    clase 27: terminada, fusion con angular

--- app/controllers/heroes.js
+++ app/controllers/heroes.js
@@ -1,28 +1,104 @@
-const HeroesController = function () {
+module.exports = function (databaseConfig) {
+
     const express = require('express');
     const router = express.Router();
 
-    let heroes = [
-        { id: 0, name: 'Profesor super o' },
-        { id: 1, name: 'Super chanqueta' },
-        { id: 2, name: 'La mujer grande' },
-        { id: 3, name: 'El amigo bueno' },
-        { id: 4, name: 'Baticosito' },
-        { id: 5, name: 'El perro grande' },
+    const TABLE = 'heroes';
 
-    ];
+    const general = require('../utils/general')();
+    //general.setDefaultDatabase('firestore');
+    let model = general.getDatabaseModel();
+    var jwt = require('jsonwebtoken');
 
-    router.get('/heroes/:id', (request, response) => {
-        let id = request.params.id;
-        let respuesta = heroes[id] == undefined ? { id: id, name: 'El heroe con id ' + id + ' no existe' } : heroes[id];
-
-        response.send(respuesta);
+    router.get('/', function (request, response) {
+        if (true)
+            model.getAll(TABLE)
+                .then((rows) => {
+                    response.send(rows);
+                }).catch((error) => {
+                    console.error(error);
+                    response.send(error);
+                });
+        else { response.send({ error: 'No se a enviado ningun token' }) };
     });
 
-    router.get('/heroes', (request, response) => {
-        response.send(heroes);
+    router.get('/:id', function (request, response) {
+        let id = request.params.id;
+        if (true)
+            model.getById(TABLE, id)
+                .then((row) => {
+                    response.send(row);
+                }).catch((error) => {
+                    console.error(error);
+                    response.send(error);
+                });
+        else { response.send({ error: 'No se a enviado ningun token' }) };
+    });
+
+
+    router.post('/options/initialize', function (request, response) {
+        if (true)
+            model.initialize(TABLE, request.body)
+                .then((message) => {
+                    response.send(message);
+                }).catch((error) => {
+                    console.error(error);
+                    response.send(error);
+                });
+        else { response.send({ error: 'No se a enviado ningun token' }) };
+    });
+
+    //{{SERVER}}/users/delete_users
+    router.get('/options/clean', function (request, response) {
+        if (true)
+            model.clean(TABLE)
+                .then((message) => {
+                    response.send(message);
+                }).catch((error) => {
+                    console.error(error);
+                    response.send(error);
+                });
+        else { response.send({ error: 'No se a enviado ningun token' }) };
+    });
+
+    //{{SERVER}}/users/
+    router.post('/', function (request, response) {
+        if (true)
+            model.create(TABLE, request.body)
+                .then((object) => {
+                    response.send(object);
+                }).catch((error) => {
+                    console.error(error);
+                    response.send(error);
+                });
+        else { response.send({ error: 'No se a enviado ningun token' }) };
+    });
+
+    router.put('/:id', function (request, response) {
+        let id = request.params.id;
+        if (true)
+            model.update(TABLE, request.body, id)
+                .then((row) => {
+                    response.send(row);
+                }).catch((error) => {
+                    console.error(error);
+                    response.send(error);
+                });
+        else { response.send({ error: 'No se a enviado ningun token' }) };
+    });
+
+    router.delete('/:id', function (request, response) {
+        let id = request.params.id;
+        if (true)
+            model.delete(TABLE, id)
+                .then((message) => {
+                    response.send(message);
+                }).catch((error) => {
+                    console.error(error);
+                    response.send(error);
+                });
+        else { response.send({ error: 'No se a enviado ningun token' }) };
     });
 
     return router;
-};
-module.exports = HeroesController;
+}
--- index.js
+++ index.js
@@ -1,38 +1,42 @@
 const express = require('express');
 const app = express();
 const config = require('./config.json');
+
 //const config = require('./app/utils/config');
-const port = process.env.PORT ? process.env.PORT : config.app.port ? config.app.port : 3000;
-const bind = process.env.BIND ? process.env.BIND : config.app.bind ? config.app.port : '127.0.0.1';
+
+const port = process.env.PORT ? process.env.PORT : config.app.port ? config.app.port : 3455;
+const bind = process.env.BIND ? process.env.BIND : config.app.bind ? config.app.bind : '127.0.0.1';
+
+//var jwt = require('jsonwebtoken');
+
+
 
 const bodyParser = require('body-parser');
-const { response, request } = require('express');
+const { request, response } = require('express');
+//const { JsonWebTokenError } = require('jsonwebtoken');
 
-//let usersController = require('./app/controllers/users_firebase')();
+let usersController = require('./app/controllers/users_firebase')();
 //let classesController = require('./app/controllers/classes')();
-//let loginController = require('./app/controllers/login')();
+let loginController = require('./app/controllers/login')();
 let heroController = require('./app/controllers/heroes')();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//app.use('/users', usersController);
+
+
+app.use('/users', usersController);
 //app.use('/classes', classesController);
-//app.use('/login', loginController);
-app.use('/api', heroController);
+app.use('/login', loginController);
+app.use('/heroes', heroController);
 
-
-
-
-app.get('/', (request, response) => {
-    response.send('Bienvenidos a la API de ' + config.app.name);
-});
+//
 
 
 
 app.listen(port, bind, function () {
-    console.log('*******************');
-    console.log('aplicacion: ' + config.app.name);
+    console.log('**********************************');
+    console.log('Aplicación: ' + config.app.name);
     console.log('corriendo en: ' + config.app.bind + ':' + config.app.port);
-    console.log('*******************');
+    console.log('**********************************');
 });
